6a35ce5bd804deff8cd63486a93c944e429945f4
kevin-vanta <kevin.royer@vanta.com>
2024-10-02 14:13:45 -0700

    update scripts

--- .zshrc
+++ .zshrc
@@ -5,7 +5,13 @@ alias gst="git status"
 
 alias hg="history | grep"
 
-export ZSH="$HOME/.oh-my-zsh"
-plugins=(git zsh-autosuggestions zsh-syntax-highlighting fast-syntax-highlighting zsh-autocomplete)
-
+export ZSH="$(HOME)/.oh-my-zsh"
+plugins=(
+    git 
+    zsh-autosuggestions 
+    zsh-syntax-highlighting 
+    fast-syntax-highlighting 
+    zsh-autocomplete
+)
+ZSH_THEME="robbyrussell"
 source $ZSH/oh-my-zsh.sh
--- install.sh
+++ install.sh
@@ -1,19 +1,35 @@
 #!/bin/bash
 
-# Install Zsh
+create_symlinks() {
+    # Get the directory in which this script lives.
+    script_dir=$(dirname "$(readlink -f "$0")")
+
+    # Get a list of all files in this directory that start with a dot.
+    files=$(find -maxdepth 1 -type f -name ".*")
+
+    # Create a symbolic link to each file in the home directory.
+    for file in $files; do
+        name=$(basename $file)
+        echo "Creating symlink to $name in home directory."
+        rm -rf ~/$name
+        ln -s $script_dir/$name ~/$name
+    done
+}
+
+echo "Creating symlinks..."
+create_symlinks
+
+echo "Installing Zsh..."
 sudo apt-get update && sudo apt-get install -y zsh
 
-# Install Oh My Zsh
+echo "Installing Oh My Zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-# Install Zsh plugins
+echo "Installing Zsh plugins..."
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone https://github.com/zdharma-continuum/fast-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/fsh
 git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/zsh-autocomplete
 
-# Copy .zshrc to home directory
-cp .zshrc ~/.zshrc
-
-# Set Zsh as default shell
+echo "Setting Zsh as default shell..."
 chsh -s $(which zsh)
