a7f1541d24b7984d774b678ba55af61542ebf9e7
kevin-vanta <kevin.royer@vanta.com>
2025-07-22 09:01:55 -0700

    fix zsh plugins, install superclaude

--- install.sh
+++ install.sh
@@ -26,6 +26,32 @@ else
     echo "tmux is already installed"
 fi
 
+echo "Installing Python dependencies..."
+if ! command -v python3 &> /dev/null; then
+    sudo apt-get update && sudo apt-get install -y python3 python3-pip
+else
+    echo "Python3 is already installed"
+fi
+
+echo "Installing uv package manager..."
+if ! command -v uv &> /dev/null; then
+    curl -LsSf https://astral.sh/uv/install.sh | sh
+    source $HOME/.cargo/env
+else
+    echo "uv is already installed"
+fi
+
+echo "Installing SuperClaude Framework..."
+# Ensure uv is in PATH for the current session
+if [ -f "$HOME/.cargo/env" ]; then
+    source $HOME/.cargo/env
+fi
+
+uv add SuperClaude
+python3 -m SuperClaude install --quick
+
+echo "SuperClaude Framework installed successfully!"
+
 echo "Installing Zsh..."
 sudo apt-get update && sudo apt-get install -y zsh
 
@@ -35,8 +61,8 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 echo "Installing Zsh plugins..."
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zdharma-continuum/fast-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/fsh
-git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/zsh-autocomplete
+git clone https://github.com/zdharma-continuum/fast-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fast-syntax-highlighting
+git clone --depth 1 -- https://github.com/marlonrichert/zsh-autocomplete.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autocomplete
 
 echo "Installing Tmux Plugin Manager..."
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
